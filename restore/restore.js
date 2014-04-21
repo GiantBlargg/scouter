759f8656e326466becabe04541d53926afd4f51c
Giantblargg <ddoran3@gmail.com>
2014-04-21 11:36:41 -0600

    added tools to run dev server

---
+++ restore/restore.js
@@ -0,0 +1,13 @@
+var reStore = require('restore'), store = new reStore.FileTree({
+    path : __dirname + '/data'
+}), server = new reStore({
+    store : store,
+    http : {
+        port : 4000
+    },
+    allow : {
+        signup : true
+    },
+});
+
+server.boot(); 
